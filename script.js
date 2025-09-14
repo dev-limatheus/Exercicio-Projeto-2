a73ff941a0089811eadb2947edddc6c0ba1be282
Limatheus <souzalima1517@gmail.com>
2025-09-14 16:56:15 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('formContato');
+const contatos = [];
+const numeros = [];
+
+ linhas=' ';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumero = document.getElementById('numero-contato');
+    {
+
+    contatos.push(inputNomeContato.value);
+    numeros.push(parseFloat(inputNumero.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumero.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+    }
+    inputNomeContato.value='';
+    inputNumero.value='';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML= linhas;  
+}
